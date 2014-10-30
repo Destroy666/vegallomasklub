d9f8d3b3239e20452ccad1692dd7eb35e376413a
razor2060 <razor2060@4a0d945f-15bd-dd81-c313-a5aee902d7eb>
2014-10-30 08:47:04 +0000

    AutoMigration disable

--- Vegallo/Migrations/Configuration.cs
+++ Vegallo/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Vegallo.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(Vegallomas.Models.vegDB context)
